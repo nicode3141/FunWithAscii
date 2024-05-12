26acb396fbb3da0e1ec5879d623f3837aba189a6
nicode3141 <nicolas.d.pfeifer@gmail.com>
2024-05-12 21:41:14 +0200

    finalized the chip thing

--- Chip.py
+++ Chip.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import time
+import winsound
+
 # OpenCV library
 
 def image_to_ascii(image_src):
@@ -20,32 +22,36 @@ def image_to_ascii(image_src):
     return ascii_image
 
 def print_image(image_src):
-    image = cv.VideoCapture(image_src)
-    frames = []
-    ret, frame = image.read()
-
-    while ret:
+    while True:
+        image = cv.VideoCapture(image_src)
+        frames = []
         ret, frame = image.read()
-        if not ret:
-            break
-        aspect_ratio = frame.shape[1] / float(frame.shape[0])
-        new_height = int(100 / aspect_ratio / 2)
-        resized_image = cv.resize(frame, (100, new_height))
 
-        ascii_chars = "@%#*+=-:. "
+        while ret:
+            ret, frame = image.read()
+            if not ret:
+                break
+            aspect_ratio = frame.shape[1] / float(frame.shape[0])
+            new_height = int(100 / aspect_ratio / 2)
+            resized_image = cv.resize(frame, (100, new_height))
 
-        ascii_image = ""
-        for row in resized_image:
-            for pixel in row:
-                ascii_image += ascii_chars[pixel / 32]
-            ascii_image += "\n"
+            ascii_chars = "@%#*+=-:. "
+            ascii_chars_inverted = " .:=+*#%@"
 
-        return ascii_image
+            ascii_image = ""
+            for row in resized_image:
+                for pixel in row:
+                    intensity = int(pixel.mean() / 255 * (len(ascii_chars) - 1)) # get median of RGB values and divide by 255
+                    ascii_image += ascii_chars_inverted[intensity]
+                ascii_image += "\n"
 
+            print(ascii_image)
+            time.sleep(0.07)
+    image.release()
 
+winsound.PlaySound("assets/funkytown.wav", winsound.SND_LOOP + winsound.SND_ASYNC)
+print_image("assets/chip.gif")
 
-image = print_image("assets/chip.gif")
-print(image)
 
 #asciiChip = image_to_ascii(r"C:\Users\nicol\PycharmProjects\FunWithAscii\assets\chip.gif")
 
